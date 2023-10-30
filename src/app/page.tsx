c51f00dd15696f28d07b82a9b86c24ba274bd7e9
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-10-30 22:15:42 +0630

    Finalized the Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,35 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="relative">
-      <NavBar />
-      <MainContainer />
-    </div>
-  );
-}
-
-const NavBar = () => {
-  return (
-    <nav className="absolute w-full my-4 px-4">
-      <div className="inline-block">
-        <Link
-          className="hover:text-white hover:bg-primary px-4 py-2 rounded-xl transition text-lg"
-          href="/"
-        >
-          Sulica
-        </Link>
+    <main className="relative h-screen overflow-hidden">
+      <div className="absolute -z-10 w-full ">
+        <Image
+          src="/hero.jpg"
+          alt="Hero Image"
+          className="h-auto w-full"
+          width={0}
+          height={0}
+          sizes="100vw"
+        />
       </div>
-      <div className="float-right inline-block">
-        <div className="flex flex-row">
-          <Link
-            className="hover:text-white hover:bg-primary px-4 py-2 rounded-xl transition text-lg"
-            href="/"
-          >
-            Quiz
-          </Link>
+      <div className="z-10 flex h-screen flex-col items-center justify-center gap-8">
+        <div className="flex flex-col gap-4 text-center">
+          <h1 className="text-4xl font-medium uppercase">
+            Welcome to Sulica&lsquo;s Webpage
+          </h1>
+          <p className="text-lg">
+            I&lsquo;m just a girl with a passion for accounting and computer
+            science
+          </p>
         </div>
-      </div>
-    </nav>
-  );
-};
 
-const MainContainer = () => {
-  return (
-    <main className="flex items-center justify-center flex-col h-screen gap-8">
-      <div className="flex flex-col gap-4 text-center">
-        <h1 className="uppercase text-4xl font-medium">
-          Welcome to Sulica&lsquo;s Webpage
-        </h1>
-        <p className="text-lg">
-          I&lsquo;m just a girl with a passion for accounting and computer
-          science
-        </p>
+        <Button className="rounded-3xl" asChild>
+          <Link href="/login">Get Started</Link>
+        </Button>
       </div>
-
-      <Button className="rounded-3xl" asChild>
-        <Link href="/login">Get Started</Link>
-      </Button>
     </main>
   );
-};
+}
